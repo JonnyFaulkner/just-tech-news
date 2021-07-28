46c807308f85b3b022a71736490eb0c530c2d817
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-07-27 21:49:14 -0500

    added password verification

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -84,4 +84,29 @@ router.delete('/:id', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(400).json({ message: 'No user with that email address!' });
+                return;
+            }
+            // Validate User
+
+            const validPassword = dbUserData.checkPassword(req.body.password);
+
+            if (!validPassword) {
+                res.status(400).json({ message: 'Incorrect password!' });
+                return;
+            }
+
+            res.json({ user: dbUserData, message: 'Your are now logged in!' });
+
+        });
+});
+
 module.exports = router;
