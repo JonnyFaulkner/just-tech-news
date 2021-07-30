4039ca21775cdd66a7c4dfe60e7e76b2cd227472
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-07-30 18:06:11 -0500

    added post routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 
+const { User, Post } = require('../models');
 const apiRoutes = require('./api');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 router.use('/api', apiRoutes);
 
