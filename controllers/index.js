97ea502fda0dce729bf0233b56e6186df26abe62
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-08-06 12:48:39 -0500

    added login and logout routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-
 const { User, Post } = require('../models');
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -10,6 +10,8 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
+
+router.use('/', homeRoutes);
 
 router.use('/api', apiRoutes);
 
